f6c15536b9b6e2fe4d77d6aef46f63f255963beb
Lindow Eric <elindow@verizon.net>
2012-04-24 15:24:14 -0400

    added loop-that-prints-odd-numbers

--- lectures/02-ruby-basics/2.1-ruby-basics-exercises/loop-that-prints-odd-numbers.rb
+++ lectures/02-ruby-basics/2.1-ruby-basics-exercises/loop-that-prints-odd-numbers.rb
@@ -1,13 +1,24 @@
-=begin
 
- Implement a loop that prints odd numbers under 10.
+#Eric Lindow
+#Assignment 2.1
 
-    1
-    3
-    5
-    7
-    9
+puts "Using loops to generate a list of odd numbers"
 
-=end
+puts "\nSimplest?"  
+(0..4).each do |i|
+puts i*2+1
+end
 
+puts "\nUsing mod operator and if"
+10.times { |i|
+	if i%2==1
+	puts "Value of variable is #{i}"
+	end
+}
+
+puts ""
+puts 'Using array'
+for i in [1,3,5,7,9]
+	puts "Array element is #{i}"
+end
 
