bffe659b59995c09caec96a3103bf680fc9ad1b9
Lindow Eric <elindow@verizon.net>
2012-04-30 23:30:54 -0400

    rack files

---
+++ lectures/03-rack/3.1-rack-basics-exercises/practice/server2.rb
@@ -0,0 +1,25 @@
+﻿require "rubygems"
+require "rack"
+
+class HelloWorld
+
+
+	def call(env)
+		#puts env.to_s
+		#puts Dir.pwd
+	begin
+		filename = env["PATH_INFO"]
+	
+		[200, {"Content type" => "text/html"}, [File.read(Dir.pwd+filename)]]
+	
+	rescue Errno::ENOENT
+			[404, {"Content type" => "text/html"}, ["File not found"]]	
+	rescue Exception=>e
+			[500, {"Content type" => "text/html"}, [e.class]]
+	
+			end
+
+	#rescue
+	end
+end
+Rack::Handler::Mongrel.run HelloWorld.new,{ :Port=>9292 }
