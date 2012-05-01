2ad6ac17b02f0dd46c674d832b7d0776bf20e896
Lindow Eric <elindow@verizon.net>
2012-04-30 23:58:15 -0400

    additional server2 code

--- lectures/03-rack/3.1-rack-basics-exercises/practice/server2.rb
+++ lectures/03-rack/3.1-rack-basics-exercises/practice/server2.rb
@@ -5,17 +5,19 @@ class HelloWorld
 
 
 	def call(env)
-		#puts env.to_s
+		puts env.to_s
 		#puts Dir.pwd
 	begin
 		filename = env["PATH_INFO"]
 	
-		[200, {"Content type" => "text/html"}, [File.read(Dir.pwd+filename)]]
+		[200, {"Content type" => "text/html"}, [filename,"\n",File.read(Dir.pwd+filename)]]
 	
+	rescue Errno::EACCES
+			[550, {"Content type" => "text/html"}, ["Permission denied"]]	
 	rescue Errno::ENOENT
 			[404, {"Content type" => "text/html"}, ["File not found"]]	
 	rescue Exception=>e
-			[500, {"Content type" => "text/html"}, [e.class]]
+			[500, {"Content type" => "text/html"}, [e,'\nError class:',e.class]]
 	
 			end
 
